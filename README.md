c2fdf86e4a8cb164d6cbefd0723f7c38904726b8
0xtr <oxtrr@protonmail.com>
2023-05-15 20:10:01 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,24 @@
+# nip95-tool
+
+*THIS IS WORK IN PROGRESS, *
+
+## Description
+A tool to decode images from kind 1064 events ([NIP-95](https://github.com/frbitten/nostr-nips/blob/NIP-95/95.md)).
+
+## Instructions
+Specify an input file that contains an array of JSON events of kind 1064 and an output folder like so:
+
+```bash
+nip95-tool -i exampleEvents.json -o files
+```
+
+To create this file, you can use the [nostr-tool](https://github.com/0xtrr/nostr-tool) CLI application like so:
+
+```bash
+nostr-tool -r wss://nostr.oxtr.dev list-events -k 1064 -o events.json
+```
+
+## Features
+- [x] Basic kind 1064 events
+- [ ] Encrypted files
+- 
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use base64;
+use clap::{Arg, Command};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::path::Path;
+use tokio::fs;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Event {
+    id: String,
+    pubkey: String,
+    created_at: u64,
+    kind: u16,
+    tags: Vec<Vec<String>>,
+    content: String,
+    sig: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let matches = Command::new("My JSON Processor")
+        .version("1.0")
+        .author("0xtr <oxtrr@protonmail.com>")
+        .about("Decodes base64 encoded files from nostr events")
+        .arg(
+            Arg::new("input")
+            .short('i')
+            .help("Input file that contains an array of kind 1064 kind events")
+            .required(true),
+        )
+        .arg(
+            Arg::new("output")
+            .short('o')
+            .help("Output directory to store the decoded files")
+            .required(true),
+        )
+        .get_matches();
+
+    // Get the input file and output directory from the command line arguments
+    let input_file = matches.get_one::<String>("input").unwrap();
+    let output_dir = matches.get_one::<String>("output").unwrap();
+
+    // Read the file to a string
+    let file_content = fs::read_to_string(input_file).await?;
+
+    // Parse the JSON content to a vector of Event
+    let data: Vec<Event> = serde_json::from_str(&file_content)?;
+
+    // Create the output directory
+    fs::create_dir_all(output_dir).await?;
+
+    for item in data {
+        // Decode the base64 content
+        let decoded_content = base64::decode(&item.content)?;
+
+        // Find the tag with type "type"
+        let filetype = item
+            .tags
+            .iter()
+            .find(|tag| tag.get(0) == Some(&"type".to_string()))
+            .and_then(|tag| tag.get(1))
+            .and_then(|s| s.split('/').nth(1))
+            .unwrap_or("txt");
+
+        // Write the decoded content to a new file in the output directory
+        let file_path = format!("{}/{}.{}", output_dir, item.id, filetype);
+        fs::write(Path::new(&file_path), &decoded_content).await?;
+    }
+
+    println!("Decoding completed successfully!");
+
+    Ok(())
+}
